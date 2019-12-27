45c56f0c51d279ca8c2ff4a9edbbe31f97c3d0a2
Matias Garcia <matiasigarcia91@gmail.com>
2019-12-27 12:21:11 +0100

    More REST routes

--- sequelize-day2-day3/index.js
+++ sequelize-day2-day3/index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 4002;
 
 const User = require("./models").User;
+const TodoList = require("./models").TodoList;
 
 app.use(express.json());
 
@@ -30,8 +31,48 @@ app.post("/users", async (req, res, next) => {
   }
 });
 
-app.post("/echo", (req, res) => {
-  res.json(req.body);
+app.put("/users/:userId", async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    const userToUpdate = await User.findByPk(userId);
+    if (!userToUpdate) {
+      res.status(404).send("User not found");
+    } else {
+      const updatedUser = await userToUpdate.update(req.body);
+      res.json(updatedUser);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get("/todoLists", async (req, res, next) => {
+  const todoLists = await TodoList.findAll();
+  res.json(todoLists);
+});
+
+app.post("/todoLists", async (req, res, next) => {
+  try {
+    const newList = await TodoList.create(req.body);
+    res.json(newList);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.put("/todoLists/:listId", async (req, res, next) => {
+  try {
+    const listId = parseInt(req.params.listId);
+    const listToUpdate = await TodoList.findByPk(listId);
+    if (!listToUpdate) {
+      res.status(404).send("List not found");
+    } else {
+      const updatedList = await listToUpdate.update(req.body);
+      res.json(updatedList);
+    }
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
