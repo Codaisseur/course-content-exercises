33b5e05811756b41d3be075d63ea09de67ea77f9
Matias Garcia <matiasigarcia91@gmail.com>
2019-12-28 16:52:52 +0100

    Add last example routes

--- sequelize-day2-day3/index.js
+++ sequelize-day2-day3/index.js
@@ -46,6 +46,83 @@ app.put("/users/:userId", async (req, res, next) => {
   }
 });
 
+app.get("/users/:userId/lists", async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId, {
+      include: [TodoList]
+    });
+    if (user) {
+      res.send(user.TodoLists);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/users/:userId/lists", async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      const newList = await TodoList.create({ userId, ...req.body });
+      res.json(newList);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Update an existing list
+app.put("/users/:userId/lists/:listId", async (req, res, next) => {
+  try {
+    const listId = parseInt(req.params.listId);
+    const toUpdate = await TodoList.findByPk(listId);
+    if (!toUpdate) {
+      res.status(404).send("List not found");
+    } else {
+      const updated = await toUpdate.update(req.body);
+      res.json(updated);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+// Delete a user's list
+app.delete("/users/:userId/lists/:listId", async (req, res, next) => {
+  try {
+    const listId = parseInt(req.params.listId);
+    const toDelete = await TodoList.findByPk(listId);
+    if (!toDelete) {
+      res.status(404).send("List not found");
+    } else {
+      const deleted = await toDelete.destroy();
+      res.json(deleted);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+// Delete all user's lists
+app.delete("/users/:userId/lists", async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId, { include: [TodoList] });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      user.TodoLists.forEach(async list => await list.destroy());
+      res.status(204).send();
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
 app.get("/todoLists", async (req, res, next) => {
   const todoLists = await TodoList.findAll();
   res.json(todoLists);
