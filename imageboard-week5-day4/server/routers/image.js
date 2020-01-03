d99d9cc179944d0ffef02d6f5e0e2cd421d58c16
Matias Garcia <matiasigarcia91@gmail.com>
2020-01-03 15:06:52 +0100

    REST example

--- imageboard-week5-day4/server/routers/image.js
+++ imageboard-week5-day4/server/routers/image.js
@@ -12,4 +12,32 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const image = await Image.findByPk(id);
+    if (!image) {
+      res.status(404).send("Image not found");
+    } else {
+      res.json(image);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { title, url } = req.body;
+    if (!title || !url) {
+      res.status(400).send("missing parameters");
+    } else {
+      const newImage = await Image.create(req.body);
+      res.json(newImage);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
--- imageboard-week5-day4/server/routers/user.js
+++ imageboard-week5-day4/server/routers/user.js
@@ -12,4 +12,18 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const { email, password, fullName } = req.body;
+    if (!email || !password || !fullName) {
+      res.status(400).send("missing parameters");
+    } else {
+      const newUser = await User.create({ email, password, fullName });
+      res.json(newUser);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
