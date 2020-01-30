913df1b3c40ff78847342603eb559606b5d02e48
Matias Garcia <matiasigarcia91@gmail.com>
2020-01-30 10:24:26 +0100

    Thunk actions and read posts

--- coders-network/src/store/rootReducer.js
+++ coders-network/src/store/rootReducer.js
@@ -1,9 +1,8 @@
 import { combineReducers } from "redux";
-
 import developers from "./developers/reducer";
-import posts from "./post/reducer";
+import postReducer from "./post/reducer";
 
 export default combineReducers({
   developers,
-  posts
+  post: postReducer
 });
