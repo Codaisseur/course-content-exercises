b7b396e29bb61f193546a084c800cf7de1cca86d
Matias Garcia <matiasigarcia91@gmail.com>
2020-03-19 19:55:01 +0100

    get all images and create image

--- week-5/imageboard-server/index.js
+++ week-5/imageboard-server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const imageRouter = require("./routers/image");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -8,6 +9,7 @@ const jsonParser = express.json();
 app.use(jsonParser);
 
 // Routers
+app.use("/images", imageRouter);
 
 // Start server
 app.listen(PORT, () => console.log("App listening"));
--- week-5/imageboard-server/routers/image.js
+++ week-5/imageboard-server/routers/image.js
@@ -1,14 +1,27 @@
 const { Router } = require("express");
 const Image = require("../models").image;
 
+// Instanciate a router
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
-  // get all images
+  try {
+    // get all images
+    const images = await Image.findAll();
+
+    // respond with the image data
+    res.json(images);
+  } catch (e) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  // create an image
+  console.log(req.body);
+  const { url, title } = req.body;
+  // create a new image
+  const newImage = await Image.create({ title, url });
+  res.json(newImage);
 });
 
 module.exports = router;
