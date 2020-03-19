aef3f0bfd98865bf7fe9a085ee06a4c07dde6912
Matias Garcia <matiasigarcia91@gmail.com>
2020-03-19 20:07:19 +0100

    validations and image by id

--- week-5/imageboard-server/routers/image.js
+++ week-5/imageboard-server/routers/image.js
@@ -4,9 +4,9 @@ const Image = require("../models").image;
 // Instanciate a router
 const router = new Router();
 
+// get all images
 router.get("/", async (req, res, next) => {
   try {
-    // get all images
     const images = await Image.findAll();
 
     // respond with the image data
@@ -16,12 +16,38 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+// create image
 router.post("/", async (req, res, next) => {
   console.log(req.body);
   const { url, title } = req.body;
+
+  if (!url || !title) {
+    res.status(406).send("missing parameters")
+  }
   // create a new image
   const newImage = await Image.create({ title, url });
   res.json(newImage);
 });
+
+// image by id
+router.get("/:id", (req, res, next) => {
+  // get id param
+  const { id } = req.params;
+  console.log("id", id);
+
+  try {
+    // Find image by id
+    const image = await Image.findByPk(id);
+    // check if image exists
+    if (!image) {
+      res.status(404).send("No image found")
+    } else {
+      // send it back
+      res.json(image);
+    }
+  } catch (e) {
+    next(e)
+  }
+})  
 
 module.exports = router;
