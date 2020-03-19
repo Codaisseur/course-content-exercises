c8403b0356381835fa286a059804adeede4c501a
Matias Garcia <matiasigarcia91@gmail.com>
2020-03-19 19:37:06 +0100

    pre-lecture clean-up

--- week-5/imageboard-server/index.js
+++ week-5/imageboard-server/index.js
@@ -1,8 +1,4 @@
 const express = require("express");
-const userRouter = require("./routers/user");
-const imageRouter = require("./routers/image");
-const authRouter = require("./routers/auth");
-const authMiddleware = require("./auth/middleware");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -12,9 +8,6 @@ const jsonParser = express.json();
 app.use(jsonParser);
 
 // Routers
-app.use("/users", userRouter);
-app.use("/images", authMiddleware, imageRouter);
-app.use("/", authRouter);
 
 // Start server
 app.listen(PORT, () => console.log("App listening"));
--- week-5/imageboard-server/routers/image.js
+++ week-5/imageboard-server/routers/image.js
@@ -1,50 +1,14 @@
 const { Router } = require("express");
-const { toData } = require("../auth/jwt");
 const Image = require("../models").image;
 
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
-  try {
-    const images = await Image.findAll();
-    res.json(images);
-  } catch (e) {
-    next(e);
-  }
+  // get all images
 });
 
 router.post("/", async (req, res, next) => {
-  try {
-    const { title, url } = req.body;
-    if (!title || !url) {
-      res.status(400).send("missing parameters");
-    } else {
-      const newImage = await Image.create(req.body);
-      res.json(newImage);
-    }
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.get("/messy", async (req, res, next) => {
-  const auth =
-    req.headers.authorization && req.headers.authorization.split(" ");
-  if (auth && auth[0] === "Bearer" && auth[1]) {
-    try {
-      console.log(auth);
-      const data = toData(auth[1]);
-      console.log(data);
-    } catch (e) {
-      res.status(400).send("Invalid JWT token");
-    }
-    const allImages = await Image.findAll();
-    res.json(allImages);
-  } else {
-    res.status(401).send({
-      message: "Please supply some valid credentials"
-    });
-  }
+  // create an image
 });
 
 module.exports = router;
